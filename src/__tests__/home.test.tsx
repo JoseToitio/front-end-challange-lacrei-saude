9d8062e735b1aed000107c66982268fb8c448953
JoseToitio <josetoitio@gmail.com>
2024-03-06 16:20:21 -0300

    test: home test

--- src/__tests__/home.test.tsx
+++ src/__tests__/home.test.tsx
@@ -2,12 +2,13 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Page from "../app/page";
 
-describe("Page", () => {
+describe("Home Page", () => {
   it("renders a heading", () => {
     render(<Page />);
 
-    const heading = screen.getAllByRole("heading", { level: 1 });
-
-    expect(heading).toHaveLength(2);
+    const title = screen.getByText("Junte-se à nossa comunidade");
+    const description = screen.getByText("Encontre atendimento clínico de qualidade ou entre para o time de profissionais da Lacrei Saúde.");
+    expect(title).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
   });
 });
